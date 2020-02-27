41ffec9adbafcec3ea39c7357ac259587e4643f6
Anton Danylov <ur.anton.danilov@gmail.com>
2020-02-27 09:21:13 +0200

    Moved feign package into controller package

--- appareldiving/adidas-data-parsing/src/main/java/com/appareldiving/adidasdataparsing/controller/feign/DataScrappingControllerProxy.java
+++ appareldiving/adidas-data-parsing/src/main/java/com/appareldiving/adidasdataparsing/controller/feign/DataScrappingControllerProxy.java
@@ -1,4 +1,4 @@
-package com.appareldiving.adidasdataparsing.feign;
+package com.appareldiving.adidasdataparsing.controller.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
