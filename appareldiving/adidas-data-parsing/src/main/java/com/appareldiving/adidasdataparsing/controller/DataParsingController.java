2dcd035439319ce738f6eeafc344fd54333040b2
Anton Danylov <ur.anton.danilov@gmail.com>
2020-02-27 09:20:33 +0200

    Added quantity in http request

--- appareldiving/adidas-data-parsing/src/main/java/com/appareldiving/adidasdataparsing/controller/DataParsingController.java
+++ appareldiving/adidas-data-parsing/src/main/java/com/appareldiving/adidasdataparsing/controller/DataParsingController.java
@@ -1,10 +1,11 @@
 package com.appareldiving.adidasdataparsing.controller;
 
-import com.appareldiving.adidasdataparsing.feign.DataScrappingControllerProxy;
+import com.appareldiving.adidasdataparsing.controller.feign.DataScrappingControllerProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,12 +15,13 @@ public class DataParsingController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private int quantity;
 
     @Autowired
     private DataScrappingControllerProxy proxy;
 
-    @GetMapping("/collect/adidas")
-    public List<String> parseData()
+    @GetMapping("/collect/{quantity}/adidas")
+    public List<String> parseData(@PathVariable int quantity)
     {
         logger.info("That's me here!");
 
