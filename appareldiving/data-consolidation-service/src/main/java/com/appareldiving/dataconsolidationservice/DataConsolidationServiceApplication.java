acf687517711a6a2d465d767340d4d38da87ba50
Anton Danylov <ur.anton.danilov@gmail.com>
2020-02-27 08:04:36 +0200

    Initial commit for data-consolidation-service

---
+++ appareldiving/data-consolidation-service/src/main/java/com/appareldiving/dataconsolidationservice/DataConsolidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.appareldiving.dataconsolidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataConsolidationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataConsolidationServiceApplication.class, args);
+    }
+
+}
---
+++ appareldiving/data-consolidation-service/src/test/java/com/appareldiving/dataconsolidationservice/DataConsolidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appareldiving.dataconsolidationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConsolidationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
